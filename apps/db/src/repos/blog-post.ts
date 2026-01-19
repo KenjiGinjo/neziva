70be77447d6b0d5b58fef1fa70ec65e9f1429648
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2026-01-19 15:55:07 +0800

    Refactor blog post retrieval logic to improve search functionality: consolidate query conditions for keyword filtering and enhance handling of default parameters. This update streamlines the query structure and ensures more efficient data retrieval based on various filters.

--- apps/db/src/repos/blog-post.ts
+++ apps/db/src/repos/blog-post.ts
@@ -28,37 +28,49 @@ export const blogPost = createRepo(db.blogPost, {
     selectForList: (q, options?: { category?: string, tag?: string, featured?: boolean, status?: EnumBlogPostStatus, keyword?: string }) => {
       const { category, tag, featured, status, keyword } = options || {}
 
-      let query = selectForDefault(q)
-
+      const query = selectForDefault(q)
+      const defaultWhere: any = { }
       // status 处理：如果未指定，默认使用 Published；如果明确指定（包括 undefined），则使用指定值
       if (status !== undefined) {
-        query = query.where({ status })
+        defaultWhere.status = status
       }
       else {
-        query = query.where({ status: EnumBlogPostStatus.Published })
+        defaultWhere.status = EnumBlogPostStatus.Published
       }
 
       if (category) {
-        query = query.where({ category })
+        defaultWhere.category = category
       }
 
       if (tag) {
-        query = query.where({ tags: { has: tag } })
+        defaultWhere.tags = { has: tag }
       }
 
       if (featured !== undefined) {
-        query = query.where({ featured })
+        defaultWhere.featured = featured
       }
 
+      const searchWhere: any = []
       if (keyword) {
-        query = query.orWhere(
-          { title: { contains: keyword } },
-          { content: { contains: keyword } },
-          { excerpt: { contains: keyword } },
-        )
+        searchWhere.push({ title: { contains: keyword } })
+        searchWhere.push({ content: { contains: keyword } })
+        searchWhere.push({ excerpt: { contains: keyword } })
       }
+      if (Object.keys(searchWhere).length > 0) {
+        const sa = searchWhere.map((item: any) => {
+          return { ...defaultWhere, ...item }
+        })
 
-      return query
+        return query.orWhere(...sa)
+      }
+      else {
+        if (Object.keys(defaultWhere).length > 0) {
+          return query.where(defaultWhere)
+        }
+        else {
+          return query
+        }
+      }
     },
 
   },
